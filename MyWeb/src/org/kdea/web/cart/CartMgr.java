33297ffb16f3de8e54a3734185a851640f159be2
tehon <anes2354@gmail.com>
2015-09-18 14:59:54 +0900

    Revert "Revert "0917 7:50""
    This reverts commit d552024ee85f808ddb6408325ddb70309618ed02.

--- MyWeb/src/org/kdea/web/cart/CartMgr.java
+++ MyWeb/src/org/kdea/web/cart/CartMgr.java
@@ -34,5 +34,40 @@ public class CartMgr {
 		cart.add(b);
 		}
 	}
-	
+	public int getTotal(){
+		int total=0;
+		
+		for(int i=0;i<cart.size();i++){
+			total+=cart.get(i).getPrice() * cart.get(i).getQty();
+		}
+		return total;
+	}
+	public boolean updatedItem(int idx,int qty){
+		try{
+			Book b = cart.get(idx);
+			b.setQty(qty);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	public boolean removeItem(int idx){
+		try{
+			cart.remove(idx);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	public boolean cleanItem(){
+		try{
+			cart.removeAll(cart);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
 }
