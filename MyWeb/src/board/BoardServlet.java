9ba3e00438193b1c1ec71fc18da671d93e2d736f
tehon <anes2354@gmail.com>
2015-09-23 09:44:14 +0900

    복구

--- MyWeb/src/board/BoardServlet.java
+++ MyWeb/src/board/BoardServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardServlet
  */
-@WebServlet("/Board?cmd=*")
+@WebServlet("/Board/*")
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
