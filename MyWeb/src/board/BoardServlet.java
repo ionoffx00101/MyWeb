1082ca0178ca357dd42ce11004513e88859ccf85
tehon <anes2354@gmail.com>
2015-09-23 09:37:03 +0900

    Revert "ss"
    This reverts commit 6ce182bd524bb33817fe8da490d6e40bfd463c8b.

--- MyWeb/src/board/BoardServlet.java
+++ MyWeb/src/board/BoardServlet.java
@@ -2,7 +2,6 @@ package board;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardServlet
  */
-@WebServlet("/Board/*")
+@WebServlet("/Board?cmd=*")
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
